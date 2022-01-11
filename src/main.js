a4e3659ee221d013462c6e41960f6b9638cee9e0
辉闪 <oncwnuPkSVjAItzwPc0ayakHbElI@git.weixin.qq.com>
2022-01-11 18:29:40 +0800

    add: vue-near

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import naive from 'naive-ui'
+import VueNear from 'vue-near'
 
 createApp(App)
     .use(store)
     .use(router)
     .use(naive)
+    .use(VueNear, {
+        // Needs the environment for the correct RPC to use
+        env: process.env.NODE_ENV || 'development',
+        // config: {
+        //   appTite: 'Cool dApp',
+        //   contractName: 'cool_dapp.testnet',
+        // },
+      })
     .mount('#app')
