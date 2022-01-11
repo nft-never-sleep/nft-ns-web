b97c9303488d1755055ad634dc46e27e789dbe16
luoliwoshang <2643523683@qq.com>
2022-01-11 17:28:35 +0800

    first push

---
+++ src/layout/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="Layout">
+    <div class="TopBar">
+      <div class="logo">NFT LEASE</div>
+      <div class="search-area">
+        <input placeholder="搜索 : 账户ID" />
+      </div>
+      <div class="connect-wallet">
+        <n-button round type="warning">connect-wallet</n-button>
+      </div>
+    </div>
+    <div class="container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Layout",
+};
+</script>
+
+<style lang="scss" scoped>
+.TopBar {
+  background: #6039ce;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  & > div {
+    display: flex;
+    justify-content: center;
+  }
+  .logo {
+    width: 183px;
+    font-size: 30px;
+    color: white;
+  }
+  .search-area {
+    flex: 1;
+    padding: 0 40px;
+    display: flex;
+    align-items: center;
+    input {
+      width: 100%;
+      height: 40px;
+      background: #eff0f6;
+    }
+  }
+  .connect-wallet {
+    width: 217px;
+    display: flex;
+    align-items: center;
+  }
+}
+.container {
+  width: 1660px;
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="Home">
+    <div class="Banner">
+      <div class="slogen">
+        <p class="p1">NFT NEVER SLEEP</p>
+        <p class="p2">NFT NEVER SLEEP</p>
+        <p class="p3">NFT NEVER SLEEP</p>
+      </div>
+      <div class="desc">
+        搜索藏家，租来使用吧｜连接你的钱包，立刻有｜往下逛逛，喜欢就报价
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+export default {};
+</script>
+<style lang="scss" scoped>
+.Banner {
+  position: relative;
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  height: 200px;
+  padding-bottom: 60px;
+
+  border-bottom: 3px solid rgba(255, 186, 13, 0.8);
+  &::after {
+    content: "";
+
+    position: absolute;
+    bottom: 0px;
+
+    transform: translate(2px, 7px);
+
+    display: block;
+    width: 100%;
+    height: 3px;
+    background-color: #7b36ff;
+  }
+  .slogen {
+    position: relative;
+    height: 100px;
+    p {
+      width: 100%;
+      font-size: 60px;
+      font-weight: 600;
+      margin: 0;
+      position: absolute;
+      top: 0;
+    }
+    .p1 {
+      position: relative;
+      z-index: 4;
+      color: #ffac32;
+    }
+    .p2 {
+      z-index: 3;
+      color: #7b36ff;
+      transform: translate(4px, 4px);
+    }
+    .p3 {
+      z-index: 2;
+      transform: translate(8px, 8px);
+      color: #ffac32;
+    }
+  }
+  .desc {
+    font-family: PingFang TC;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 32px;
+    line-height: 34px;
+    /* or 106% */
+
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    color: #898397;
   }
 }
-</script>
+</style>
